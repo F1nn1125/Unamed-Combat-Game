98f36c23259434307c73026b6e6ebe30ee40b9a2
F1nn1125 <71734783+F1nn1125@users.noreply.github.com>
2022-08-10 12:22:41 +1200

    some work done on skill points

--- mainloop.py
+++ mainloop.py
@@ -14,40 +14,49 @@ charmana = 5
 charstr = 5
 characc = 5
 charagil = 5
-print("Allocate {} skill points to your attributes in order: ".format(skillpoints))
+print("Allocate {} skill points to your attributes of Health, Stamina, Mana, Strength, Accuracy and Agility: ".format(skillpoints))
 while True:
     try:
-        charhp = int(input("- HP = {}".format(charhp)))
+        skilladd = int(input("- HP = {}".format(charhp)))
+        if skilladd < 0:
+            print("enter a number above 0")
+        skillpoints = skillpoints - skilladd
+        if skillpoints < 0:
+            print("You cannot allocate this many!")
+            skillpoints = skilladd
+            skilladd = 0
+        else:
+            charhp = charhp + skilladd
         break
     except:
         print("enter a number")
 while True:
     try:
-        charstam = int(input("- Stamina = {}".format(charstam)))
+        charstam = charstam + int(input("- Stamina = {}".format(charstam)))
         break
     except:
         print("enter a number")
 while True:
     try:
-        charmana = int(input("- Mana = {}".format(charmana)))
+        charmana = charmana + int(input("- Mana = {}".format(charmana)))
         break
     except:
         print("enter a  number")
 while True:
     try:
-        charstr = int(input("- Strength = {}".format(charstr)))
+        charstr = charstr + int(input("- Strength = {}".format(charstr)))
         break
     except:
         print("enter a number")
 while True:
     try:
-        characc = int(input("- Accuracy = {}".format(characc)))
+        characc = characc + int(input("- Accuracy = {}".format(characc)))
         break
     except:
         print("enter a number")
 while True:
     try:
-        charagil = int(input("- Agility = {}".format(charagil)))
+        charagil = charagil + int(input("- Agility = {}".format(charagil)))
         break
     except:
         print("enter a number")
