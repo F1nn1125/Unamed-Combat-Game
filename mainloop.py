21e9ca215ab8ec2d30556366dc0cea8d9df14bae
F1nn1125 <71734783+F1nn1125@users.noreply.github.com>
2022-08-12 10:57:08 +1200

    added class and race calculation
    I am well aware of my shitty code please ignore it

--- mainloop.py
+++ mainloop.py
@@ -2,27 +2,103 @@ from random import randint as ri
 import itemvariables as iv
 import startgame
 
+# Preset Variables
 level = 1
-
-#Character maker
-charrace = input("""
-Choose a race:
-1. Orc (health & damage scaling)
-2. Elf (mana & agility scaling)
-3. Human (accuracy & stamina scaling)
-""")
-charclass = input("""
-1. Fighter (mele damage scaling)
-2. Mage (mana scaling)
-3. Brute (hitpoint scaling)
-""")
 skillpoints = 15
+
+# Allocated skill points
 charhp = 5
 charstam = 5
 charmana = 5
 charstr = 5
 characc = 5
 charagil = 5
+
+# Recalculated stats
+
+recalc_mana = charmana * (level + 1)
+recalc_hp = charhp * (level + 1)
+recalc_stam = charstam * (level +1)
+recalc_str = charstr * (level + 1)
+recalc_acc = characc * (level +1)
+recalc_agil = charagil * (level + 1)
+
+# Character maker
+while True:
+    while True:
+        try:
+            charrace = int(input("""
+            Choose a race:
+            1. Orc (health & damage scaling)
+            2. Elf (mana & agility scaling)
+            3. Human (accuracy & stamina scaling)
+            """))
+            break
+        except:
+            print("enter 1, 2 or 3")
+    while True:
+        try:
+            charclass = int(input("""
+            1. Fighter (mele damage scaling)
+            2. Mage (mana scaling)
+            3. Brute (hitpoint scaling)
+            """))
+            break
+        except:
+            print("enter 1, 2 or 3")
+
+            # Ignore my stupid code here i know theres a better way to do this
+    if charrace <= 0 or charrace > 3:
+        print(" enter either 1, 2 or 3")
+        continue
+    elif charclass <= 0 or charclass > 3:
+        print("enter either 1, 2 or 3")
+        continue
+
+    #More stupid code please ignore
+    elif charrace == 1 and charclass == 1:
+        recalc_hp = charhp * (level + 5)
+        recalc_str = charstr * (level + 10)
+        break
+    elif charrace == 1 and charclass == 2:
+        recalc_hp = charhp * (level + 5)
+        recalc_str = charstr * (level + 5)
+        recalc_mana = charmana * (level + 5)
+        break
+    elif charrace == 1 and charclass == 3:
+        recalc_hp = charhp * (level + 10)
+        recalc_str = charstr * (level + 5)
+        break
+    elif charrace == 2 and charclass == 1:
+        recalc_str = charstr * (level + 5)
+        recalc_mana = charmana * (level + 5)
+        recalc_agil = charagil * (level + 5)
+        break
+    elif charrace == 2 and charclass == 2:
+        recalc_mana = charmana * (level + 10)
+        recalc_agil = charagil * (level + 5)
+        break
+    elif charrace == 2 and charclass == 3:
+        recalc_mana = charmana * (level + 5)
+        recalc_agil = charagil * (level + 5)
+        recalc_hp = charhp * (level + 5)
+        break
+    elif charrace == 3 and charclass == 1:
+        recalc_acc = characc * (level + 5)
+        recalc_stam = charstam * (level + 5)
+        recalc_str = charstr * (level + 5)
+        break
+    elif charrace == 3 and charclass == 2:
+        recalc_acc = characc * (level + 5)
+        recalc_stam = charstam * (level + 5)
+        recalc_mana = charmana * (level + 5)
+        break
+    elif charrace == 3 and charclass == 3:
+        recalc_acc = characc * (level + 5)
+        recalc_stam = charstam * (level + 5)
+        recalc_hp = charhp * (level + 5)
+        break
+    # The shitty code is over (not really)
 
 #Skillpoint allocation
 def skillpointallocation():
